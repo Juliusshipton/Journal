dc49a4a3fef993ed58f564ed879f25baca816079
Julius Shipton <Julius Shipton@DESKTOP-75I7BMU>
2021-06-05 12:40:53 -0700

    Started framework to refactor queries from controller classes.  All queries will now be within the Queries class.

---
+++ FitnessJournal/Data/Queries.cs
@@ -0,0 +1,22 @@
+﻿using FitnessJournal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessJournal.Data
+{
+    /*
+     * Helper class for common queries to refactor code from controllers. 
+     */
+    public class Queries
+    {
+        // Queries for meal controller.
+        public class Meal
+        {
+            public static List<MealIngredient> GetTempMealIngredients(JournalDbContext _context) => 
+                _context.MealIngredient.Where(m => m.Meal.Name.Equals("$$$$_TEMP_MEAL_$$$$")).ToList();
+            
+        }
+    }
+}
